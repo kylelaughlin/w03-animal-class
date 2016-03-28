141ae1acd30abe45d55a747b5006b93f25cfc8f5
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-28 10:20:48 -0500

    Add documentation to the fish initialize method

--- lib/fish.rb
+++ lib/fish.rb
@@ -4,6 +4,13 @@ class Fish
 
   attr_accessor :species, :length, :color
 
+  # Construct the initial state of a fish object
+  #
+  # +species: a string representing the species of a fish
+  # +length: an integer representing the length of a fish
+  # +color: a string represetning the color of a fish
+  #
+  # Returns the fish object
   def initialize(species:, length:, color:)
     @species = species
     @length = length
